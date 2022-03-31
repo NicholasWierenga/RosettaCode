59662ffc4f1b447cf4d41819615ce235676a9b1b
Nicholas Wierenga <102650184+NicholasWierenga@users.noreply.github.com>
2022-03-31 01:57:52 -0400

    Add files via upload

--- Palindromic Primes.sql
+++ Palindromic Primes.sql
@@ -1,12 +1,11 @@
 SET SERVEROUTPUT ON;
 
 CREATE TABLE primelist (
-    prime NUMBER(6),
-    tabrows NUMBER(6) DEFAULT seq1.NextVal
+    prime   NUMBER(6),
+    tabrows NUMBER(6) DEFAULT seq1.NEXTVAL
 );
 
-CREATE SEQUENCE seq1
-  INCREMENT BY 1;
+CREATE SEQUENCE seq1 INCREMENT BY 1;
   
 /*
 This will be my submission for the Palindromic Primes task on rosettacode.org. This
@@ -21,7 +20,8 @@ BEGIN
         FOR y IN 2..n LOOP
             IF y = n THEN
                 primenum := n;
-                INSERT INTO primelist (prime) VALUES (primenum);
+                INSERT INTO primelist ( prime ) VALUES ( primenum );
+
             ELSIF n MOD y = 0 THEN
                 EXIT;
             END IF;
@@ -31,34 +31,48 @@ END;
 
 DECLARE -- This filters through our table for palindromes.
     testpalprime INTEGER(6);
-    x INTEGER(6) := 1;
-    rowsremain INTEGER(6);
-    currrow INTEGER(6);
+    x            INTEGER(6) := 1;
+    rowsremain   INTEGER(6);
+    currrow      INTEGER(6);
 BEGIN
-    SELECT COUNT(*) INTO rowsremain FROM primelist;
-    SELECT seq1.CURRVAL INTO currrow FROM DUAL;
-    currrow := currrow - rowsremain + 1;
+    SELECT COUNT(*)
+    INTO rowsremain
+    FROM primelist;
 
-WHILE x <= rowsremain LOOP
-    
-    SELECT prime INTO testpalprime FROM primelist WHERE tabrows = currrow;
-    
-        FOR c IN 1..CEIL(LENGTH(testpalprime)/2) LOOP
-            IF SUBSTR(testpalprime, c, 1) != SUBSTR(testpalprime, -c, 1) THEN
-                
-                DELETE FROM primelist WHERE prime = testpalprime;
+    SELECT seq1.CURRVAL
+    INTO currrow
+    FROM dual;
+
+    currrow := currrow - rowsremain + 1;
+    WHILE x <= rowsremain LOOP
+        SELECT prime
+        INTO testpalprime
+        FROM primelist
+        WHERE tabrows = currrow;
+
+        FOR c IN 1..ceil(length(testpalprime) / 2) LOOP
+            IF substr(testpalprime, c, 1) != substr(testpalprime, -c, 1) THEN
+                DELETE FROM primelist
+                WHERE prime = testpalprime;
+
                 x := x - 1;
-                exit; --When a non-palindrome is found, that row is deleted.
-            
+                EXIT; --When a non-palindrome is found, that row is deleted.
+
             END IF;
         END LOOP;
-    x := x + 1;
-    currrow := currrow + 1;
-    SELECT COUNT(*) INTO rowsremain FROM primelist;
+
+        x := x + 1;
+        currrow := currrow + 1;
+        SELECT COUNT(*)
+        INTO rowsremain
+        FROM primelist;
+
     END LOOP;
+
 END; -- What's left in the table are palindromic primes.
 
-SELECT PRIME AS "Palindromic Primes" FROM primelist;
+SELECT prime AS "Palindromic Primes"
+FROM primelist;
 
 DROP SEQUENCE seq1;
 
