3693c662e731e83c0c8ed7619a5e95ee324bed24
Nicholas Wierenga <102650184+NicholasWierenga@users.noreply.github.com>
2022-04-01 00:36:17 -0400

    Add files via upload

--- Palindromic Primes.sql
+++ Palindromic Primes.sql
@@ -1,40 +1,34 @@
-SET SERVEROUTPUT ON;
+/*
+This will be my submission for the Palindromic Primes task on rosettacode.org
+for PL/SQL. The task is to find all palindromic prime numbers below 1000.
+PL/SQL isn't need for this and can be done simpler and faster with SQL.
+http://rosettacode.org/wiki/Palindromic_primes
+*/
+CREATE SEQUENCE seq1 INCREMENT BY 1;
 
 CREATE TABLE primelist (
     prime   NUMBER(6),
     tabrows NUMBER(6) DEFAULT seq1.NEXTVAL
 );
 
-CREATE SEQUENCE seq1 INCREMENT BY 1;
-  
-/*
-This will be my submission for the Palindromic Primes task on rosettacode.org. This
-is currently unsolved for PL/SQL. The task is, find all palindromic prime numbers
-below 1000. http://rosettacode.org/wiki/Palindromic_primes
-*/
-
-DECLARE -- This is to populate the table with primes in a given range.
-    primenum INTEGER(6);
+DECLARE -- This works by filling a table with primes, then removes non-palindromes.
+    testpalprime INTEGER(6);
+    reverseprime INTEGER(6);
+    x            INTEGER(6) := 1;
+    rowsremain   INTEGER(6);
+    currrow      INTEGER(6);
 BEGIN
-    FOR n IN 1..1000 LOOP
+    FOR n IN 1..1000 LOOP  -- This is to populate the table with primes in a given range.
         FOR y IN 2..n LOOP
             IF y = n THEN
-                primenum := n;
-                INSERT INTO primelist ( prime ) VALUES ( primenum );
+                INSERT INTO primelist ( prime ) VALUES ( n );
 
             ELSIF n MOD y = 0 THEN
                 EXIT;
             END IF;
         END LOOP;
     END LOOP;
-END;
 
-DECLARE -- This filters through our table for palindromes.
-    testpalprime INTEGER(6);
-    x            INTEGER(6) := 1;
-    rowsremain   INTEGER(6);
-    currrow      INTEGER(6);
-BEGIN
     SELECT COUNT(*)
     INTO rowsremain
     FROM primelist;
@@ -45,28 +39,25 @@ BEGIN
 
     currrow := currrow - rowsremain + 1;
     WHILE x <= rowsremain LOOP
-        SELECT prime
-        INTO testpalprime
+        SELECT prime,
+               reverse(to_char(prime))
+        INTO
+            testpalprime,
+            reverseprime
         FROM primelist
         WHERE tabrows = currrow;
 
-        FOR c IN 1..ceil(length(testpalprime) / 2) LOOP
-            IF substr(testpalprime, c, 1) != substr(testpalprime, -c, 1) THEN
-                DELETE FROM primelist
-                WHERE prime = testpalprime;
+        IF testpalprime <> reverseprime THEN
+            DELETE FROM primelist --When a non-palindrome is found, that row is deleted.
+            WHERE prime = testpalprime;
 
-                x := x - 1;
-                EXIT; --When a non-palindrome is found, that row is deleted.
+            rowsremain := rowsremain - 1;
+            x := x - 1; -- Subtracts 1 so no numbers are skipped over.
 
-            END IF;
-        END LOOP;
+        END IF;
 
         x := x + 1;
         currrow := currrow + 1;
-        SELECT COUNT(*)
-        INTO rowsremain
-        FROM primelist;
-
     END LOOP;
 
 END; -- What's left in the table are palindromic primes.
@@ -76,4 +67,4 @@ FROM primelist;
 
 DROP SEQUENCE seq1;
 
-DROP TABLE primelist;
+DROP TABLE primelist PURGE;
