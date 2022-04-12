2e8eb43efa21745cd4756a013bd6b7a1f585b6ba
Nicholas Wierenga <102650184+NicholasWierenga@users.noreply.github.com>
2022-04-11 20:45:59 -0400

    Removed Delete Statement
    It was unnecessary and if someone wants a table of primes or palindromic primes they should use the PLSQL method.

--- SQL - Palindromic Primes.sql
+++ SQL - Palindromic Primes.sql
@@ -1,26 +1,30 @@
 /*
-This will be my submission for the Palindromic Primes task on rosettacode.org
+This is my attempt at the Palindromic Primes task on rosettacode.org
 for SQL. The task is to find all palindromic prime numbers below 1000.
+This code is relatively fast, but struggles when the table is large, due to
+the subquery.
 http://rosettacode.org/wiki/Palindromic_primes
 */
-CREATE TABLE primelist (
-    nums NUMBER(6)
+CREATE TABLE numlist (
+    nums INTEGER
 );
 
-INSERT INTO primelist ( nums )
+INSERT INTO numlist ( nums )
     SELECT ROWNUM
-    FROM col$ -- Pick some large-rowed table or start cross joining if you need more rows for some reason.
-    FETCH FIRST 1000 ROWS ONLY; -- Change to expand range.
+    FROM col$
+    CROSS JOIN (SELECT ROWNUM FROM col$ FETCH FIRST 100 ROWS ONLY)
+    FETCH FIRST 1800000 ROWS ONLY; -- Making numlist larger than it needs to be raises query time a lot.
 
-DELETE FROM primelist a 
-WHERE EXISTS ( -- We use a correlated subquery to delete non-primes.
-    SELECT b.nums 
-    FROM primelist b
-    WHERE mod(a.nums, b.nums) = 0 AND a.nums > b.nums AND b.nums <> 1
-) OR a.nums = 1;
+SELECT nums AS "Palindromic Primes"
+FROM numlist a
+WHERE NOT EXISTS (SELECT b.nums 
+    FROM numlist b
+    WHERE mod(a.nums, b.nums) = 0 AND a.nums > b.nums AND b.nums <> 1) AND a.nums <> 1  -- Checks if a.nums is prime.
+    AND nums = reverse(to_char(nums)) -- Checks if we have a palindrome.
+    AND a.nums BETWEEN 1 and 1800000; -- Defines our range.
 
-SELECT nums AS "Palindromic Primes" -- Takes the primes and searches for the palindromes.
-FROM primelist
-WHERE nums = reverse(to_char(nums));
+DROP TABLE numlist PURGE;
 
-DROP TABLE primelist PURGE;
+-- 1.8 million rows took about a minute to sort through. Another way exists  
+-- involving PLSQL that works far faster can be found here:
+-- https://github.com/NicholasWierenga/RosettaCode/blob/main/SQL%20-%20Palindromic%20Primes.sql
